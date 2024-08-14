922043ff6e86b66d302919b12d0e5cf41ef5c0fc
Nguyen Chi Hoang <chihoang.nguyen2709@gmail.com>
2024-08-14 21:20:22 +0700

    My second commit with 50 60% progress

--- Desktop/Mini_project/src/services/databaseService.js
+++ Desktop/Mini_project/src/services/databaseService.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectMongoDB = async () => {
     try {
-        mongoose.connect("mongodb+srv://chihoangnguyen2709:hoang2003@cluster0.3ugeoqe.mongodb.net/OnlineStore?retryWrites=true&w=majority&appName=Cluster0")
+        mongoose.connect("connect string")
         console.log('connected to mongoDB');
     } catch(err) {
         console.error('Error connecting to MongoDB: ', err);
